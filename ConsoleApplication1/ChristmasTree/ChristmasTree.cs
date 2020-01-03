18ee13a82deaca7f26feb0b1f57dae89117825a3
pankajbstech <49518338+pankajbstech@users.noreply.github.com>
2020-01-03 14:37:25 +0530

    tree

---
+++ ConsoleApplication1/ChristmasTree/ChristmasTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.ChristmasTree
+{
+    class ChristmasTree
+    {
+        static void Main()
+        {
+            int spaces = 10;
+            int asterix = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < asterix; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                asterix++;
+                spaces--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
