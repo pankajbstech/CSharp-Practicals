2bb0111943063a8d9ff291d3db99cc23127bd914
pankajbstech <49518338+pankajbstech@users.noreply.github.com>
2020-01-16 20:16:39 +0530

    Done Generic

---
+++ ConsoleApplication1/Generic/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Generic
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            bool equal = Calculator.AreEqual(1, 2);
+            if (equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+                Console.WriteLine("Not Equal");
+            Console.ReadLine();
+        }
+    }
+    class Calculator
+    {
+        public static bool AreEqual(string Value1, string Value2)
+        {
+            return Value1 == Value2;
+        }
+        public static bool AreEqual(object Value1, object Value2)
+        {
+            return Value1 == Value2;
+        }
+        public static bool AreEqual<T>(T Value1, T Value2)// Generic Method
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+    class Calculator<T>  // Generic Class
+    {
+        public static bool AreEqual(string Value1, string Value2)
+        {
+            return Value1 == Value2;
+        }
+        public static bool AreEqual(object Value1, object Value2)
+        {
+            return Value1 == Value2;
+        }
+        public static bool AreEqual(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+}
