29745e844542da96b533bff0093f793e125aef9d
pankajbstech <49518338+pankajbstech@users.noreply.github.com>
2020-01-17 09:48:55 +0530

    generic

---
+++ ConsoleApplication1/Interface/InterfaceImplementation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Interface
+{
+    interface ICustomer
+    {
+        //int Id;
+        //public void Print();
+        void Print();
+    }
+    public class Customer : ICustomer
+    {
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class InterfaceImplementation
+    {
+    }
+}
